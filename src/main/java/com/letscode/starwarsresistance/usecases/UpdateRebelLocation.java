2dfb17ba3196fca55dd95d7ae1b6ff6daba69e95
Paulo <deoliveira.pe@gmail.com>
2022-03-12 21:39:01 -0300

    feat: rebel and location validation added. Test updated. LocationValidator class added

--- src/main/java/com/letscode/starwarsresistance/usecases/UpdateRebelLocation.java
+++ src/main/java/com/letscode/starwarsresistance/usecases/UpdateRebelLocation.java
@@ -2,18 +2,29 @@ package com.letscode.starwarsresistance.usecases;
 
 import com.letscode.starwarsresistance.domains.entities.Location;
 import com.letscode.starwarsresistance.domains.entities.Rebel;
+import com.letscode.starwarsresistance.exceptions.BusinessValidatationException;
 import com.letscode.starwarsresistance.gateways.persistence.RebelPersistenceGateway;
+import com.letscode.starwarsresistance.usecases.validators.CreateRebelValidator;
+import com.letscode.starwarsresistance.usecases.validators.LocationValidator;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UpdateRebelLocation {
+    private final CreateRebelValidator createRebelValidator;
+    private final LocationValidator locationValidator;
     private final RebelPersistenceGateway rebelPersistenceGateway;
 
     public Rebel execute(Rebel rebel, Location location){
-        //location valid
-        //rebel exists
+
+        val locationErrors = locationValidator.validate(location);
+        if (!locationErrors.isEmpty()) throw new BusinessValidatationException(locationErrors);
+
+        val rebelErrors = createRebelValidator.validate(rebel);
+        if (!rebelErrors.isEmpty()) throw new BusinessValidatationException(rebelErrors);
+
         rebel.setLocation(location);
         return rebelPersistenceGateway.save(rebel);
     }
--- src/test/java/com/letscode/starwarsresistance/usecases/UpdateRebelLocationTest.java
+++ src/test/java/com/letscode/starwarsresistance/usecases/UpdateRebelLocationTest.java
@@ -2,13 +2,18 @@ package com.letscode.starwarsresistance.usecases;
 
 import com.letscode.starwarsresistance.domains.entities.Location;
 import com.letscode.starwarsresistance.domains.entities.Rebel;
+import com.letscode.starwarsresistance.exceptions.BusinessValidatationException;
 import com.letscode.starwarsresistance.gateways.persistence.RebelPersistenceGateway;
+import com.letscode.starwarsresistance.usecases.validators.CreateRebelValidator;
+import com.letscode.starwarsresistance.usecases.validators.LocationValidator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoSettings;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,15 +22,51 @@ class UpdateRebelLocationTest {
 
     @InjectMocks
     UpdateRebelLocation updateRebelLocation;
+    @Mock LocationValidator locationValidator;
+    @Mock CreateRebelValidator createRebelValidator;
     @Mock RebelPersistenceGateway rebelPersistenceGateway;
 
     @Test
     void shouldUpdateWhenValidationSucceeds() {
+
         Rebel rebel = Rebel.builder().build();
         Location location = Location.builder().build();
+
         Assertions.assertNotEquals(rebel.getLocation(), location);
+
+        Mockito.when(locationValidator.validate(location)).thenReturn(List.of());
+        Mockito.when(createRebelValidator.validate(rebel)).thenReturn(List.of());
+
         updateRebelLocation.execute(rebel, location);
-        Mockito.verify(rebelPersistenceGateway).save(rebel);
         Assertions.assertEquals(rebel.getLocation(), location);
+
+        Mockito.verify(rebelPersistenceGateway).save(rebel);
+    }
+
+    @Test
+    void shouldNotUpdateWhenRebelValidationFails() {
+
+        Rebel rebel = Rebel.builder().build();
+        Location location = Location.builder().build();
+
+        Assertions.assertNotEquals(rebel.getLocation(), location);
+
+        Mockito.when(locationValidator.validate(location)).thenReturn(List.of());
+        Mockito.when(createRebelValidator.validate(rebel)).thenReturn(List.of("rebel validation error"));
+
+        Assertions.assertThrows(BusinessValidatationException.class, () -> updateRebelLocation.execute(rebel, location));
+    }
+
+    @Test
+    void shouldNotUpdateWhenLocationValidationFails() {
+
+        Rebel rebel = Rebel.builder().build();
+        Location location = Location.builder().build();
+
+        Assertions.assertNotEquals(rebel.getLocation(), location);
+
+        Mockito.when(locationValidator.validate(location)).thenReturn(List.of("location validation error"));
+
+        Assertions.assertThrows(BusinessValidatationException.class, () -> updateRebelLocation.execute(rebel, location));
     }
 }
