c9e404a08c1ca730e387c47dcb49b66df32088f7
Paula Lustosa <pl.lustosa@gmail.com>
2022-03-24 16:07:22 -0300

    Add excluded Put REST requesting (reportRebelAsTraitor)

--- src/main/java/com/letscode/starwarsresistance/gateways/controller/RebelController.java
+++ src/main/java/com/letscode/starwarsresistance/gateways/controller/RebelController.java
@@ -4,6 +4,7 @@ import com.letscode.starwarsresistance.domains.entities.Location;
 import com.letscode.starwarsresistance.domains.entities.Rebel;
 import com.letscode.starwarsresistance.gateways.persistence.RebelPersistenceGateway;
 import com.letscode.starwarsresistance.usecases.CreateRebel;
+import com.letscode.starwarsresistance.usecases.ReportRebelAsTraitor;
 import com.letscode.starwarsresistance.usecases.UpdateRebelLocation;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -12,8 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import javax.print.attribute.standard.Media;
-
 @RestController
 @RequestMapping(value = "/rebels")
 @RequiredArgsConstructor
@@ -21,9 +20,10 @@ public class RebelController {
 
   private final Logger log = LoggerFactory.getLogger(RebelController.class.getName());
   private final RebelMapper rebelMapper;
-  private final CreateRebel createRebel;
   private final LocationMapper locationMapper;
+  private final CreateRebel createRebel;
   private final UpdateRebelLocation updateRebelLocation;
+  private final ReportRebelAsTraitor reportRebelAsTraitor;
   private final RebelPersistenceGateway rebelPersistenceGateway;
 
   @PostMapping(
@@ -54,4 +54,18 @@ public class RebelController {
     Rebel rebelSaved = updateRebelLocation.execute(rebel, location);
     return new RebelResponse(rebelSaved);
   }
+
+  @PutMapping(path = "report-traitor/{id}",
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseStatus(HttpStatus.OK)
+  public RebelResponse reportRebelAsTraitor(@PathVariable("id") Long id, @RequestBody RebelRequest rebelRequest) {
+    log.info("Reporting as traitor: {}", rebelRequest);
+    Rebel rebel = rebelMapper.toRebel(rebelRequest);
+    rebel.setId(id);
+    log.info("Reporting as traitor: {}", rebel);
+    Rebel rebelSaved = reportRebelAsTraitor.execute(rebel);
+    return new RebelResponse(rebelSaved);
+  }
+
 }
